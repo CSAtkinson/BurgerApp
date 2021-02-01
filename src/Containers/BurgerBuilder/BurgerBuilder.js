ad758f3381e9def1e145aa6f6f2cd1024671cb5a
James Atkinson <CSAtkinson8588@gmail.com>
2021-02-01 16:11:20 -0500

    "More" button functionality working

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,53 @@
 import React, {Component} from 'react'
 
 import Auxillary from '../../hoc/Auxillary'
+import Burger from '../../Components/Burger/Burger'
+import BuildControls from '../../Components/Burger/BuildControls/BuildControls'
+
+const INGREDIENT_PRICE = {
+    salad: .5,
+    cheese: .4,
+    meat: 1.3,
+    bacon: .7
+}
 
 class BurgerBuilder extends Component {
+    
+    
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type]
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        }
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICE[type];
+        const oldprice = this.state.totalPrice;
+        const newPrice = oldprice + priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+    }
+
+    removeIngredientHandler = (type) => {
+
+    }
+
+
     render(){
         return(
             <Auxillary>
-                <div>Burger</div>
-                <div>Build COntrols</div>
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildControls 
+                ingredientAdded = {this.addIngredientHandler}/>
             </Auxillary>
         );
     }
