19bafcdade15bacd5dc2c918494a522dd0435b95
James Atkinson <CSAtkinson8588@gmail.com>
2021-02-01 12:21:42 -0500

    Initial commit with some code implemented

---
+++ src/Components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Auxillary from '../../hoc/Auxillary'
+import classes from './Layout.module.css'
+
+const layout = (props) => (
+    <Auxillary>
+        <div> Toolbar. SideDrawer, Backdroup</div>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
+    </Auxillary>
+);
+
+export default layout
