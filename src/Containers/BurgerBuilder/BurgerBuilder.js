283f6d1c4e257d8bbbf833aaa8640d85f8293cfa
James Atkinson <CSAtkinson8588@gmail.com>
2021-02-12 16:56:36 -0500

    Added url and query params

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -86,31 +86,41 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        //alert('You Continue')
-        this.setState({ loading: true })
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'James Aktinson',
-                address: {
-                    street: 'My Street',
-                    zipCode: '5875664',
-                    country: 'USA'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
+        // //alert('You Continue')
+        // this.setState({ loading: true })
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'James Aktinson',
+        //         address: {
+        //             street: 'My Street',
+        //             zipCode: '5875664',
+        //             country: 'USA'
+        //         },
+        //         email: 'test@test.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         //console.log(response)
+        //         this.setState({ loading: false, purchasing: false });
+        //     })
+        //     .catch(error => {
+        //         //console.log(error);
+        //         this.setState({ loading: false, purchasing: false });
+        //     });
+
+        const queryParams = [];
+        for (let i in this.state.ingredients){
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-        axios.post('/orders.json', order)
-            .then(response => {
-                //console.log(response)
-                this.setState({ loading: false, purchasing: false });
-            })
-            .catch(error => {
-                //console.log(error);
-                this.setState({ loading: false, purchasing: false });
-            });
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: "/checkout",
+            search: '?' + queryString
+        });
     }
 
     render() {
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import BrowserRouter from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const app = (
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+)
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
